5e8917a8f906dd75c7c9142cc999dc258a8aa3f1
Abhishek sah <sah755146@gmail.com>
2021-01-07 16:11:06 +0530

    added prod website

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get("/callback", function (req, res) {
         // we can also pass the token to the browser to make requests from there
         res.redirect(
           prod
-            ? "http://abhisheksah.netlify.app/#"
+            ? "http://abhisheksah.netlify.app/"
             : "http://localhost:3000/#" +
                 querystring.stringify({
                   access_token: access_token,
